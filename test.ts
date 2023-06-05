39a009449fe8250cb59bd1a250c94ea3a9cd84b9
ProDev <84587419+absdevx@users.noreply.github.com>
2023-06-05 19:30:34 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -8,26 +8,31 @@
 |
 | For the most part you will never edit this file. The configuration
 | for the tests can be controlled via ".adonisrc.json" and
-| "tests/bootstrap.ts" files.
+| "tests/bootstrap.ts" files. Make data more secured
 |
 */
 
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = "test";
 
-import 'reflect-metadata'
-import sourceMapSupport from 'source-map-support'
-import { Ignitor } from '@adonisjs/core/build/standalone'
-import { configure, processCliArgs, run, RunnerHooksHandler } from '@japa/runner'
+import "reflect-metadata";
+import sourceMapSupport from "source-map-support";
+import { Ignitor } from "@adonisjs/core/build/standalone";
+import {
+  configure,
+  processCliArgs,
+  run,
+  RunnerHooksHandler,
+} from "@japa/runner";
 
-sourceMapSupport.install({ handleUncaughtExceptions: false })
+sourceMapSupport.install({ handleUncaughtExceptions: false });
 
-const kernel = new Ignitor(__dirname).kernel('test')
+const kernel = new Ignitor(__dirname).kernel("test");
 
 kernel
   .boot()
-  .then(() => import('./tests/bootstrap'))
+  .then(() => import("./tests/bootstrap"))
   .then(({ runnerHooks, ...config }) => {
-    const app: RunnerHooksHandler[] = [() => kernel.start()]
+    const app: RunnerHooksHandler[] = [() => kernel.start()];
 
     configure({
       ...kernel.application.rcFile.tests,
@@ -38,8 +43,8 @@ kernel
         setup: app.concat(runnerHooks.setup),
         teardown: runnerHooks.teardown,
       },
-      cwd: kernel.application.appRoot
-    })
+      cwd: kernel.application.appRoot,
+    });
 
-    run()
-  })
+    run();
+  });
