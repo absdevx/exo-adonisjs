b5f05b374b9a1dc57b3aade576c92845c25ec0ac
ProDev <84587419+absdevx@users.noreply.github.com>
2023-06-09 13:00:46 +0200

    Adding a script to publish feature quickly

--- update_feature.sh
+++ update_feature.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Argument parsing
-OPTIONS=$(getopt -o f:c:n: --long folder:,commit:,feature: -n 'publish_feature.sh' -- "$@")
+OPTIONS=$(getopt -o c:n: --long commit: -n 'update_feature.sh' -- "$@")
 if [ $? -ne 0 ]; then
   echo "Invalid arguments provided."
   exit 1
@@ -9,21 +9,13 @@ fi
 eval set -- "$OPTIONS"
 
 # Default values
-folder=""
 commit=""
-feature=""
 
 # Process command-line options
 while true; do
   case "$1" in
-    -f | --folder )
-      folder="$2"
-      shift 2 ;;
     -c | --commit )
       commit="$2"
-      shift 2 ;;
-    -n | --feature )
-      feature="$2"
       shift 2 ;;
     -- )
       shift
@@ -34,32 +26,22 @@ while true; do
   esac
 done
 
-# Validate required arguments
-if [ -z "$folder" ] || [ -z "$commit" ] || [ -z "$feature" ]; then
-  echo "Usage: publish_feature.sh -f <folder> -c <commit> -n <feature>"
+if [ -z "$commit" ] || [ -z "$FEATURE_NAME" ]; then
+  echo "Usage: $0 -c <commit>"
   exit 1
 fi
 
-# Arguments
-folder="$1"
-commit="$2"
-feature="$3"
 
-echo "commit=$commit"
-return
-
-# Navigate to the specified folder
-cd "$folder" || exit
 git add "."
 
 # Commit the changes with the provided message
 git commit -am "$commit"
 
 # Publish the feature branch using Git Flow
-git flow feature publish "$feature"
+git flow feature publish "$FEATURE_NAME"
 
 # Publish the feature branch using Git Flow
-git flow feature track origin "$feature"
+git flow feature track origin "$FEATURE_NAME"
 
 
-echo "Feature '$feature' published successfully."
+echo "Feature '$FEATURE_NAME' published successfully."
